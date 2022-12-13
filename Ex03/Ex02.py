6a082b76f7501f0408ce0d7cb451f4f3ee5a8c33
NEVA <neva.vka@yandex.ru>
2022-12-13 20:16:11 +0300

    Задание 3

--- Ex03/Ex02.py
+++ Ex03/Ex02.py
@@ -1,10 +1,13 @@
 x = int(input("Введите координаты х: "))
 y = int(input("Введите координаты y: "))
 
-if x > 0 and y > 0:
+if x > 0 and y > 0 and x !=0 and y != 0 :
     print("1 четверть")
-elif x < 0 and y > 0:
+elif x < 0 and y > 0 and x !=0 and y != 0:
     print("2 четверть")
-elif x < 0 and y < 0:
+elif x < 0 and y < 0 and x !=0 and y != 0:
     print("3 четверть")
-else: print("4 четверть")
+elif x > 0 and y < 0 and x !=0 and y != 0: 
+    print("4 четверть")
+else: print("Лежит на оси")
+    
